3e56057133d40b845f6bcc896d48fb4df4dde73a
oeryx9 <vmet0443@gmail.com>
2025-05-22 09:43:09 -0700

    Add files via upload

---
+++ multiplication (1).c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int a;
+    int b;
+    printf("enter the number:\n");
+    scanf("%d",&a);
+    
+    printf("enter the second number to be multiplied:\n");
+    scanf("%d",&b);
+    
+    printf("%d",a*b);
+    
+    
+    
+    
+    return 0;
+}
+
