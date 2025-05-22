42d11e9227af93e7691aaf3947979049f2962337
oeryx9 <vmet0443@gmail.com>
2025-05-22 09:35:59 -0700

    Add files via upload

---
+++ all.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf("enter the number:\n");
+    scanf("%d",&a);
+    printf("%d\n",a*1);
+    printf("%d\n",a*2);
+    printf("%d\n",a*3);
+    printf("%d\n",a*4);
+    printf("%d\n",a*5);
+    printf("%d\n",a*6);
+    
+    
+    
+    
+    
+    
+    return 0;
+}
+
