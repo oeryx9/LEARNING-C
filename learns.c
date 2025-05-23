fd70168e8e86791578bfeedeecd7c71c17897e4a
oeryx9 <vmet0443@gmail.com>
2025-05-23 10:07:45 -0700

    Add files via upload

---
+++ learns.c
@@ -0,0 +1,42 @@
+#Include<stdio.h>
+int main(){
+    /*
+    logical operators:
+    AND (&&)
+    OR(||)
+    NOT(!)
+    
+    arithmetic operators:
+    +
+    -
+    *
+    /
+    %
+    
+    relational operators:    
+    ==                        
+    !=
+    >
+    <
+    >=
+    <=
+    
+    if/else, if/elseif
+    
+    char=''
+    char[]=""
+    int=%d - &
+    char=%char
+    float=%f
+    /*
+
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+
+}
